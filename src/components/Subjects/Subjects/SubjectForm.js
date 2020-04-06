c87b00a63566816e95e8e1b288049861f9d98f8e
Stephen McDonald <stephenmcdonald8787@gmail.com>
2020-04-06 10:14:43 -0400

    refactored submitForm func in Subject form to handle updates as well, edits for subject now calls new backend resource path

--- src/components/Subjects/Subjects/SubjectForm.js
+++ src/components/Subjects/Subjects/SubjectForm.js
@@ -2,38 +2,29 @@ import React, { useState, useEffect, useContext } from "react";
 import ApiContext from "../../../contexts/ApiContext";
 import SubjectContext from "../../../contexts/SubjectContext";
 
-function SubjectForm(props) {
+export default function SubjectForm(props) {
   let { API, user, Auth } = useContext(ApiContext),
     { getSubjects } = useContext(SubjectContext),
+    // Signifies update if subject is passed via props
     { subject } = props,
     nameValue = subject ? subject.navName : "",
     descValue = subject ? subject.subjectDesc : "",
     [name, setName] = useState(nameValue),
     [desc, setDesc] = useState(descValue),
     [submitting, setSubmitting] = useState(false);
-  useEffect(() => {
-    if (user) {
-      Auth.currentSession().then(v => {
-        console.log("currentSession");
-        console.log(v);
-      });
-    }
-  }, []);
 
-  // <h3>Create Subject to Study</h3>
   return (
     <div className="form-component">
-      <form >
+      <form>
         <label>
           <span>Subject's Name</span>
-
           <textarea
             type="text"
             className="form-textarea"
             onChange={e => setName(e.target.value)}
             value={name}
             disabled={nameValue ? true : false}
-            placeholder="Subject's Name or Book Title"
+            placeholder="Subject's Name"
           />
         </label>
         <label>
@@ -46,17 +37,14 @@ function SubjectForm(props) {
             placeholder="Brief Description"
           />
         </label>
-        <button
-          type="button"
-          disabled={submitting}
-          onClick={!subject ? submitForm : updateForm}
-        >
-          {!submitting ? "submit" : "submitting"}
+        <button type="button" disabled={submitting} onClick={submitForm}>
+          {!submitting ? "Submit" : "Submitting"}
         </button>
       </form>
     </div>
   );
   function startWithLetter(str) {
+    // Returns True if arg starts with a letter
     return str
       .trim()
       .toLowerCase()
@@ -65,11 +53,25 @@ function SubjectForm(props) {
       : false;
   }
   async function submitForm() {
+    // Temporarily assumes user is creating subject
+    let apiMethod = "post",
+      // path for backend api
+      resourcePath = `/users/${user.username}/subjects`;
+    // subject being passed via props signifies update
+    if (subject) {
+      // Updates api method for edits
+      apiMethod = "put";
+      // Appends pathName for edits
+      resourcePath += `/${subject.pathName}`;
+    }
+    // Validates form inputs
     if (!startWithLetter(name) || !startWithLetter(desc))
       return alert("Name and Description must begin with a letter");
+    // Confirms user is signed in
+    if (!user || !user.username) return alert("Must Sign In");
+    // Disables submit button
     setSubmitting(true);
-    if (!user || !user.username) return  alert("Must Sign In")
-    return await API.post("StuddieBuddie", `/users/${user.username}/subjects`, {
+    return await API[apiMethod]("StuddieBuddie", resourcePath, {
       body: JSON.stringify({
         subjectName: name.trim(),
         subjectDesc: desc.trim()
@@ -86,31 +88,9 @@ function SubjectForm(props) {
       })
       .catch(error => {
         setSubmitting(false);
-        alert(error);
-        console.log(error);
-        console.log(error.response);
-      });
-  }
-
-  function updateForm() {
-    if (!startWithLetter(name) || !startWithLetter(desc))
-      return alert("Name and Description must begin with a letter");
-    setSubmitting(true);
-    API.put("StuddieBuddie", "/subjects", {
-      body: JSON.stringify({
-        username: user.username,
-        pathName: subject.pathName,
-        subjectDesc: desc
-      })
-    })
-      .then(response => {
-        console.log(response);
-        getSubjects().then(() => setSubmitting(false));
-      })
-      .catch(error => {
+        alert(error.response);
+        console.log('error');
         console.log(error.response);
       });
   }
 }
-
-export default SubjectForm;
