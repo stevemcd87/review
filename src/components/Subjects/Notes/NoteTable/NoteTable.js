c19f5baf70908ace98851abe168ff6be21975396
Stephen McDonald <stephenmcdonald8787@gmail.com>
2020-04-11 17:30:30 -0400

    noteTable working correctly, next resize textarea and display it after saving to database

--- src/components/Subjects/Notes/NoteTable/NoteTable.js
+++ src/components/Subjects/Notes/NoteTable/NoteTable.js
@@ -4,17 +4,20 @@ import "./NoteTable.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
 
-export default function NoteTable({setTable}) {
+export default function NoteTable({ setTable }) {
   let [columnHeaders, setColumnHeaders] = useState([]),
     [dataRows, setDataRows] = useState([]);
   useEffect(() => {
-    addColumn()
+    addColumn();
   }, []);
   useEffect(() => {
     console.log("columnHeaders");
     console.log(columnHeaders);
+    // checkTableIds("ch", columnHeaders);
   }, [columnHeaders]);
+
   useEffect(() => {
+    // checkTableIds("dr", dataRows);
     console.log("dataRows");
     console.log(dataRows);
   }, [dataRows]);
@@ -48,7 +51,9 @@ export default function NoteTable({setTable}) {
             return (
               <tr key={`dr-${ind}`}>
                 <td>
-                  <button type="button" onClick={e=>removeDataRow(ind)}>X</button>
+                  <button type="button" onClick={e => removeDataRow(ind)}>
+                    X
+                  </button>
                 </td>
                 {tr.map((t, i) => {
                   return (
@@ -76,7 +81,7 @@ export default function NoteTable({setTable}) {
                   <button
                     type="button"
                     disabled={a.length === 1 && i === 0}
-                    onClick={() => removeColumn(i)}
+                    onClick={() => removeColumn(v.id)}
                   >
                     <FontAwesomeIcon icon={faTrash} />
                   </button>
@@ -89,85 +94,162 @@ export default function NoteTable({setTable}) {
     </div>
   );
 
+  function checkTableIds(str, arr) {
+    switch (str) {
+      case "ch":
+      console.log("isCHValidbefore");
+      console.log(isCHValid());
+        if (!isCHValid()) updateCH();
+        console.log("isCHValidafter");
+        console.log(isCHValid());
+        break;
+      case "dr":
+        console.log("isDRValidbefore");
+        console.log(isDRValid());
+        if (!isDRValid()) updateDRIds();
+        console.log("isDRValid after");
+        console.log(isDRValid());
+        break;
+      default:
+        alert("wrong parameter");
+    }
+
+    function isCHValid() {
+      return arr.every((c, i) => +c.id === i);
+    }
+    function isDRValid() {
+      return arr.every((r, ri) => r.every((d, di) => d.id === `${ri}-${di}`));
+    }
+
+    function updateDRIds() {
+      console.log("updated DR");
+      let updated = arr.slice().map((r, ri) => {
+        return r.map((d, di) => {
+          d.id = `${ri}-${di}`;
+          return d;
+        });
+      });
+
+      console.log(updated);
+      setDataRows(updated);
+    }
+
+    function updateCH() {
+      console.log("updated CH");
+      let updated = arr.slice().map((v, i) => {
+        v.id = i;
+        return v;
+      });
+
+      console.log(updated);
+      setColumnHeaders(updated);
+    }
+  }
+
   function addColumn() {
     let clone = columnHeaders.slice(),
-    id = clone.length === 0 ? 0 + "" : +clone[clone.length - 1].id + 1 + "";
+      id = clone.length === 0 ? 0 + "" : +clone[clone.length - 1].id + 1 + "";
     clone.push({
       inputValue: "",
       id: id
     });
-    console.log(clone);
     setColumnHeaders(clone);
-    updateDataRows();
-
+    updateDataRows(clone);
   }
 
-  function updateDataRows(indexToRemove=null){
+  function updateDataRows(ch, chIdToRemove) {
+    console.log("updateDataRows");
+    console.log("chIdToRemove");
+    console.log(chIdToRemove);
     let dataRowsClone = dataRows.slice(),
-      updateDataRows = indexToRemove ? removeDataColumn() : addDataColumn();
-    setDataRows(updateDataRows);
+      updated =
+        chIdToRemove
+          ? removeDataColumn()
+          : addDataColumn();
+    console.log("ch");
+    console.log(ch);
+    console.log(updated);
+    setDataRows(updated);
 
-    function removeDataColumn(){
-      return dataRowsClone.map((dr)=>{
-        dr.splice(indexToRemove,1);
-        return dr
-      })
+    function removeDataColumn() {
+      console.log("removeDataColumn");
+      console.log(dataRowsClone);
+      return dataRowsClone.map(dr => {
+        console.log(dr);
+        let u =dr.filter((d)=>!d.id.endsWith(chIdToRemove));
+        return u
+      });
     }
-    function addDataColumn(){
-      return dataRowsClone.map((v,i)=>{
+    function addDataColumn() {
+      console.log("add Data column");
+      return dataRowsClone.filter((v, i) => {
         v.push({
           inputValue: "",
-          id:  i + "-" + columnHeaders.length
-        })
-        return v
-      })
+          id: Date.now() + "-" + i + "-" + ch[ch.length-1].id
+        });
+        return v;
+      });
     }
   }
 
-  function removeColumn(indexToRemove) {
-    let columnHeadersClone = columnHeaders.slice(),
+  function removeColumn(idToRemove) {
+    console.log("remove column");
+    let updatedCH = columnHeaders.filter(v=>v.id !== idToRemove),
       dataRowsClone = dataRows.slice();
-    columnHeadersClone.splice(indexToRemove,1);
-    setColumnHeaders(columnHeadersClone);
-    updateDataRows(indexToRemove);
+      // idRemoved = updatedCH.splice(indexToRemove, 1)[0].id;
+      console.log("idToRemove");
+      console.log(idToRemove);
+      // console.log(updatedCH.splice(indexToRemove, 1));
+    // if (indexToRemove !== columnHeaders.length - 1) {
+    //   let updated = updateIds(updatedCH, dataRowsClone);
+    //   updatedCH = updated.newCH;
+    //   dataRowsClone = updated.newDR;
+    // }
+    setColumnHeaders(updatedCH);
+    updateDataRows(columnHeaders, idToRemove);
   }
 
-  function updateColumn(id, inputValue){
+  function updateColumn(id, inputValue) {
     let clone = columnHeaders.slice(),
-      index = clone.findIndex(ch=>ch.id === id);
+      index = clone.findIndex(ch => ch.id === id);
     clone[index].inputValue = inputValue;
     setColumnHeaders(clone);
   }
 
-
   function addDataRow() {
     let clone = dataRows.slice(),
       row = [
-      ...Array(columnHeaders.length)
-        .fill("")
-        .map((v, i) => {
-          return {
-            inputValue: "",
-            id: clone.length  + "-" + i
-          };
-        })
-    ];
+        ...Array(columnHeaders.length)
+          .fill("")
+          .map((v, i) => {
+            return {
+              inputValue: "",
+              id: Date.now() + "-"+ i + "-" + columnHeaders[i].id
+            };
+          })
+      ];
     clone.push(row);
-    setDataRows(clone)
+    setDataRows(clone);
   }
 
   function removeDataRow(indexToRemove) {
     let clone = dataRows.slice();
-    clone.splice(indexToRemove,1);
+    clone.splice(indexToRemove, 1);
     setDataRows(clone);
   }
 
-  function updateDataRowInput(id, inputValue){
+  function updateDataRowInput(id, inputValue) {
+    console.log("updateDataRowInput");
     let clone = dataRows.slice(),
-      indexes = id.split('-'),
-      row = +indexes[0],
-      column = +indexes[1]
-    clone[row][column].inputValue = inputValue;
+      input;
+      clone.find((val)=>{
+        input = val.find(v=> v.id=== id)
+        return input
+    });
+      // indexes = id.split("-"),
+      // row = +indexes[0],
+      // column = +indexes[1];
+    input.inputValue = inputValue;
     setDataRows(clone);
   }
 }
@@ -183,28 +265,31 @@ function TableData({ inputValue, id, updateTableData }) {
         spellCheck={true}
         aria-label={id}
       />
-    <Preview val={inputValue}/>
-  </>
+      <Preview val={inputValue} />
+    </>
   );
 }
 
-function Preview ({val}){
+function Preview({ val }) {
   let [prev, setPrev] = useState(),
     [sup, setSup] = useState();
 
-  useEffect(()=>{
+  useEffect(() => {
     setSup(val.match(/(?<=<sup>).*(?=<\/sup>)/));
-  },[val])
+  }, [val]);
 
-  useEffect(()=>{
-    if(sup){
-      setPrev(val.replace(/<sup>.*<\/sup>/, ""))
+  useEffect(() => {
+    if (sup) {
+      setPrev(val.replace(/<sup>.*<\/sup>/, ""));
     } else {
-      setPrev(null)
+      setPrev(null);
     }
-  },[sup])
+  }, [sup]);
 
   return (
-    <span>{prev}<sup>{sup}</sup></span>
-  )
+    <span>
+      {prev}
+      <sup>{sup}</sup>
+    </span>
+  );
 }
