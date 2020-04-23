10e35311dbd37b44287b742de23827bd03b77aed
Stephen McDonald <stephenmcdonald8787@gmail.com>
2020-04-23 19:09:57 -0400

    added a log to debug on mobile

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
     };
 
   useEffect(() => {
-    console.log(auth);
+    console.log(Auth);
     setUser(Auth.user);
     // Checks authState whether or not to hide the greeting
     let hd =
