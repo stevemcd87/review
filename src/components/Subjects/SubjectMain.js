e2d44c60cac80200e7c33f30845bf0b3cb6e98e1
Stephen McDonald <stephenmcdonald8787@gmail.com>
2020-07-28 09:51:45 -0400

    removed css

--- src/components/Subjects/SubjectMain.js
+++ src/components/Subjects/SubjectMain.js
@@ -3,7 +3,7 @@ import Subjects from "./Subjects/Subjects";
 import SubjectDetail from "./SubjectDetail/SubjectDetail";
 import Category from "./Category/Category";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./SubjectMain.css";
+// import "./SubjectMain.css";
 function SubjectMain() {
   return (
     <Router>
@@ -23,19 +23,5 @@ function SubjectMain() {
     </Router>
   );
 }
-
-// <nav>
-//   <ul>
-//     <li>
-//       <Link to="/">Home</Link>
-//     </li>
-//     <li>
-//       <Link to="/about">About</Link>
-//     </li>
-//     <li>
-//       <Link to="/users">Users</Link>
-//     </li>
-//   </ul>
-// </nav>
 
 export default SubjectMain;
