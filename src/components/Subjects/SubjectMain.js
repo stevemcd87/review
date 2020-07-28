dbac55ac0d93b42f3c683e28c174c762dfaf981f
Stephen McDonald <stephenmcdonald8787@gmail.com>
2020-07-28 11:25:32 -0400

    Return an exact path to home with 'hey'

--- src/components/Subjects/SubjectMain.js
+++ src/components/Subjects/SubjectMain.js
@@ -6,25 +6,32 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./SubjectMain.css";
 function SubjectMain() {
   return (
-    <h1>hey</h1>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <h1>hey</h1>
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-// <Router>
-//   <div>
-//     <Switch>
-//       <Route path="/:username/:subjectName/:categoryName">
-//         <Category />
-//       </Route>
-//       <Route path="/:username/:subjectName">
-//         <SubjectDetail />
-//       </Route>
-//       <Route exact path="/">
-//         <Subjects />
-//       </Route>
-//     </Switch>
-//   </div>
-// </Router>
+// <Route path="/:username/:subjectName/:categoryName">
+//   <Category />
+// </Route>
+// <Route path="/:username/:subjectName">
+//   <SubjectDetail />
+// </Route>
+// <Route exact path="/">
+//   <Subjects />
+// </Route>
+//
+
+
+
 
 
 export default SubjectMain;
